5953d74e827c234ae3d94fa172472807411f2c32
Usama Nasar <usamanasar38@gmail.com>
2020-07-19 18:59:56 +0500

    add tailwind css

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button class="btn btn-blue">
+      Clicke me
+    </button>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {}
+    }
+  },
+  variants: {},
+  plugins: []
+};
